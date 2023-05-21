f696b0dca99707d86a70313a8e01d53271eecddc
Prince Kumar <princesansaraj@gmail.com>
2023-05-21 12:28:00 +0530

    Sample news added

--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class Newsitems extends Component {
   render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
-        this os news item.
+      <div className='my-3'>
+        <div className="card" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsUrl} target='blank' className="btn btn-sm btn-primary">Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
