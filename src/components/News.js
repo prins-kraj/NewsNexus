e40e2e165b410cc1a8fccb23bdf4dbbeaa46cca0
Prince Kumar <princesansaraj@gmail.com>
2023-05-21 23:09:45 +0530

    next & previous buttin added

--- src/components/News.js
+++ src/components/News.js
@@ -13,11 +13,38 @@ export class News extends Component {
   }
 
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8935beed65a44170978c421f7a4aaa87";
+    let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8935beed65a44170978c421f7a4aaa87&page=1&pagesize=20";
     let data = await fetch(url);
     let parseData = await data.json();
     console.log(parseData);
-    this.setState({articles: parseData.articles});
+    this.setState({articles: parseData.articles, totalResults: parseData.totalResults });
+  }
+
+  handlePrevClick =async () =>{
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8935beed65a44170978c421f7a4aaa87&page=${this.state.page-1}&pagesize=20`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({
+      page: this.state.page-1,
+      articles: parseData.articles
+    })
+  }
+
+  handleNextClick = async () =>{
+    if (this.state.page+1 > Math.ceil(this.state.totalResults/20)) {
+      
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8935beed65a44170978c421f7a4aaa87&page=${this.state.page+1}&pagesize=20`;
+      let data = await fetch(url);
+      let parseData = await data.json();
+      console.log(parseData);
+      this.setState({
+        page: this.state.page+1,
+        articles: parseData.articles
+      })
+    }
   }
 
   render() {
@@ -30,6 +57,10 @@ export class News extends Component {
               <Newsitems title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button disabled={this.state.page<=1} type="button" class="btn btn-primary" onClick={this.handlePrevClick} >&larr; Previous</button>
+          <button type="button" class="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
